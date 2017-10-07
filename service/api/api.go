1dd517893581debb723d1aa3d54e0aa9c17f28b6
625524105@qq.com <625524105@qq.com>
2017-10-07 17:43:39 +0800

    good commit

--- service/api/api.go
+++ service/api/api.go
@@ -12,13 +12,12 @@ import (
 )
 
 func call(w http.ResponseWriter, r *http.Request) {
-	client, err := micro.NewServiceClient("com-shendu-service-usercenter-user", pb.NewSdUserClient)
+	client, err := micro.NewServiceClient("com.shendu.service.usercenter.user", pb.NewSdUserClient)
 	if err != nil {
 		log.Println(err.Error())
 		return
 	}
 	log.Println("Successfully created service client => ", client.Config.Name)
-	log.Println("New service client succeed. service host => ", client.Config.Host, " port => ", client.Config.Port)
 	resp, err := client.Call("GetUserInfo", context.TODO(), &pb.UserReq{Id: 123456})
 	if err != nil {
 		log.Println(err)
@@ -36,13 +35,9 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("I am the hi API."))
 	})
-	http.HandleFunc("/hiapi/call", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("In call...")
-		call(w, r)
-	})
 	http.HandleFunc("/call", func(w http.ResponseWriter, r *http.Request) {
 		call(w, r)
 	})
-	log.Println("Hi api server running on port: 6767")
+	log.Println("Hi api server running on port: 8989")
 	http.ListenAndServe(":8989", nil)
 }
